7b12455c8ceec85cf4d2336dfcc47c332a909d41
Nicolas Krul <nicolaskrul@gmail.com>
2016-07-27 19:52:24 +0200

    Added menu component

--- heimdall/src/app/app.component.ts
+++ heimdall/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
+import { MenuComponent} from './navigation/menu/menu.component'
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
-  directives: [ROUTER_DIRECTIVES]
+  directives: [ROUTER_DIRECTIVES, MenuComponent]
 })
 export class Dashboard {
   title = 'app works!';
