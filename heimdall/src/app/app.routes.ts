864e6aa0fe7bd29fe88bd15dabc33d8b5085b1c2
Nicolas Krul <nicolaskrul@gmail.com>
2016-07-28 19:43:34 +0200

    MenuComponent integrated

--- heimdall/src/app/app.routes.ts
+++ heimdall/src/app/app.routes.ts
@@ -1,11 +1,12 @@
-import { provideRouter, RouterConfig } from '@angular/router';
+import {provideRouter, RouterConfig } from '@angular/router';
 
 import {LandingspageComponent} from './landingspage/landingspage.component';
 import {ProfileComponent} from './profile/profile.component';
 
 const routes: RouterConfig = [
   { path: 'profile', component: ProfileComponent },
-  { path: 'landingspage', component: LandingspageComponent }
+  { path: 'landingspage', component: LandingspageComponent },
+  { path: '', component: LandingspageComponent}
   ];
 
   export const RouterProviders = [
--- heimdall/src/app/navigation/menu/menu.component.ts
+++ heimdall/src/app/navigation/menu/menu.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 
 @Component({
   moduleId: module.id,
   selector: 'menu',
   templateUrl: 'menu.component.html',
-  styleUrls: ['menu.component.css']
+  styleUrls: ['menu.component.css'],
+  directives:[ROUTER_DIRECTIVES]
 })
 export class MenuComponent implements OnInit {
 
