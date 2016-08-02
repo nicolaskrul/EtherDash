16b579a04e6bf168fa6dfd8384fd16228ef96da7
Nicolas Krul <nicolaskrul@gmail.com>
2016-08-02 23:43:56 +0200

    Added mock cards to pages

--- heimdall/src/app/landingspage/landingspage.component.ts
+++ heimdall/src/app/landingspage/landingspage.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {CardUnitComponent} from '../tools/card-unit/card-unit.component';
 
 @Component({
   moduleId: module.id,
   selector: 'app-landingspage',
   templateUrl: 'landingspage.component.html',
-  styleUrls: ['landingspage.component.css']
+  styleUrls: ['landingspage.component.css'],
+  directives: [CardUnitComponent]
 })
 export class LandingspageComponent implements OnInit {
 
--- heimdall/src/app/profile/profile.component.ts
+++ heimdall/src/app/profile/profile.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {CardUnitComponent} from '../tools/card-unit/card-unit.component';
 
 @Component({
   moduleId: module.id,
   selector: 'app-profile',
   templateUrl: 'profile.component.html',
-  styleUrls: ['profile.component.css']
+  styleUrls: ['profile.component.css'],
+directives: [CardUnitComponent]
 })
 export class ProfileComponent implements OnInit {
 
