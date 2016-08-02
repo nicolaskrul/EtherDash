df88919b75d00afc193a842f792836b2498e929f
Nicolas Krul <nicolaskrul@gmail.com>
2016-08-02 21:01:07 +0200

    TItle binding done. restyling...

--- heimdall/src/app/tools/card-unit/card-unit.component.ts
+++ heimdall/src/app/tools/card-unit/card-unit.component.ts
@@ -4,14 +4,14 @@ import { Component, Input } from '@angular/core';
   moduleId: module.id,
   selector: 'card-unit',
   templateUrl: 'card-unit.component.html',
-  styleUrls: ['card-unit.component.css'],
-  inputs:['cardTitle']
+  styleUrls: ['card-unit.component.css']
 })
 export class CardUnitComponent {
-
+    @Input('card-title') cardTitle;
     public title;
+
     ngOnInit() {
-        title : this.cardTitle;
+        this.title = this.cardTitle;
     };
 
 }
