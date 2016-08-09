a2dfe572320c6d41eb7d3c32bdde58339277db13
Nicolas Krul <nicolaskrul@gmail.com>
2016-08-10 00:06:16 +0200

    Extra files for login functionality

--- heimdall/src/app/app.component.ts
+++ heimdall/src/app/app.component.ts
@@ -1,15 +1,22 @@
+import { AuthService} from './login/auth.service';
 import { Component } from '@angular/core';
+import { LoginComponent} from './login/login.component';
+import { MenuComponent} from './navigation/menu/menu.component';
 import { ROUTER_DIRECTIVES } from '@angular/router';
-import { MenuComponent} from './navigation/menu/menu.component'
-import { LoginComponent} from './login/login.component'
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
-  directives: [ROUTER_DIRECTIVES, MenuComponent]
+  directives: [ROUTER_DIRECTIVES, MenuComponent],
+  providers:[AuthService]
 })
 export class Dashboard {
 
+    constructor(private _service:AuthService){}
+
+    logout(){
+        this._service.logout();
+    }
 }
