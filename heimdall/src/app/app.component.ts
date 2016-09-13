8f3aad0ecf1e2554bb91f03f6d95813e420092f8
Nicolas Krul <nicolaskrul@gmail.com>
2016-09-14 00:45:37 +0200

    sidebar added to root

--- heimdall/src/app/app.component.ts
+++ heimdall/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { Component } from '@angular/core';
 import { LoginComponent} from './auth/login/login.component';
 import { MenuComponent} from './navigation/menu/menu.component';
 import { ROUTER_DIRECTIVES } from '@angular/router';
+import {SideMenuComponent} from './navigation/side-menu/side-menu.component';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
-  directives: [ROUTER_DIRECTIVES, MenuComponent]
+  directives: [ROUTER_DIRECTIVES, MenuComponent, SideMenuComponent]
 
 })
 export class Dashboard {
--- heimdall/src/app/dashboard/dashboard.component.ts
+++ heimdall/src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {CardUnitComponent} from '../tools/card-tools/card-unit/card-unit.component';
 import {CardPanelComponent} from '../tools/card-tools/card-panel/card-panel.component';
-import {SideMenuComponent} from '../navigation/side-menu/side-menu.component';
+
 
 
 @Component({
@@ -9,7 +9,7 @@ import {SideMenuComponent} from '../navigation/side-menu/side-menu.component';
   selector: 'dashboard',
   templateUrl: 'dashboard.component.html',
   styleUrls: ['dashboard.component.css'],
-  directives: [CardUnitComponent, CardPanelComponent, SideMenuComponent]
+  directives: [CardUnitComponent, CardPanelComponent]
 })
 export class DashboardComponent implements OnInit {
 
